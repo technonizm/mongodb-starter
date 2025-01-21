8864be05c40766ad1b895f96300226f4677a0ddd
technonizm <technonizm@gmail.com>
2025-01-22 00:16:22 +0300

    Update 404.tsx

--- pages/404.tsx
+++ pages/404.tsx
@@ -3,7 +3,7 @@ import Meta from '@/components/layout/meta';
 export default function Custom404() {
   return (
     <>
-      <Meta title="404 – Sayfa Bulunamadı" />
+      <Meta props={{ title: "404 – Sayfa Bulunamadı" }} />
       <div className="flex flex-col items-center justify-center min-h-screen text-center">
         <h1 className="text-4xl font-bold">404</h1>
         <p className="mt-4">Aradığınız sayfa bulunamadı.</p>
